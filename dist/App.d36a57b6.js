d4c64989075b9a7302bfdc8492339f43450dcacf
hraya <hraya@lssu.edu>
2019-08-30 21:14:41 -0500

    Added Components
    also added API call to get information on pets

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -32283,7 +32283,58 @@ if (undefined === "mock") {
 }
 
 module.exports.ANIMALS = require("./animals");
-},{"./impl":"../node_modules/@frontendmasters/pet/impl.js","./animals":"../node_modules/@frontendmasters/pet/animals.js"}],"useDropdown.js":[function(require,module,exports) {
+},{"./impl":"../node_modules/@frontendmasters/pet/impl.js","./animals":"../node_modules/@frontendmasters/pet/animals.js"}],"Pet.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Pet;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Pet(_ref) {
+  let name = _ref.name,
+      animal = _ref.animal,
+      breed = _ref.breed;
+  return _react.default.createElement("div", null, _react.default.createElement("h1", null, name), _react.default.createElement("h2", null, animal), _react.default.createElement("h2", null, breed));
+}
+
+;
+},{"react":"../node_modules/react/index.js"}],"Results.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _Pet = _interopRequireDefault(require("./Pet"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const Results = (_ref) => {
+  let pets = _ref.pets;
+  return _react.default.createElement("div", {
+    className: "search"
+  }, pets.length === 0 ? _react.default.createElement("h1", null, "No Pets Found") : pets.map(pet => _react.default.createElement(_Pet.default, {
+    animal: pet.type,
+    key: pet.id,
+    name: pet.name,
+    breed: pet.breeds.primary,
+    media: pet.photos,
+    location: `${pet.contact.address.city}, ${pet.contact.address.state}`,
+    id: pet.id
+  })));
+};
+
+var _default = Results;
+exports.default = _default;
+},{"react":"../node_modules/react/index.js","./Pet":"Pet.js"}],"useDropdown.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -32303,34 +32354,26 @@ function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = 
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-var useDropdown = function useDropdown(label, defaultState, options) {
-  var _useState = (0, _react.useState)(defaultState),
-      _useState2 = _slicedToArray(_useState, 2),
-      state = _useState2[0],
-      setState = _useState2[1];
+const useDropdown = (label, defaultState, options) => {
+  const _useState = (0, _react.useState)(defaultState),
+        _useState2 = _slicedToArray(_useState, 2),
+        state = _useState2[0],
+        setState = _useState2[1];
 
-  var id = "use-dropdown-".concat(label.replace(" ", "").toLowerCase());
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
 
-  var Dropdown = function Dropdown() {
-    return _react.default.createElement("label", {
-      htmlFor: id
-    }, label, _react.default.createElement("select", {
-      id: id,
-      value: state,
-      onChange: function onChange(e) {
-        return setState(e.target.value);
-      },
-      onBlur: function onBlur(e) {
-        return setState(e.target.value);
-      },
-      disabled: options.legnth === 0
-    }, _react.default.createElement("option", null, "All"), options.map(function (item) {
-      return _react.default.createElement("option", {
-        key: item,
-        value: item
-      }, item);
-    })));
-  };
+  const Dropdown = () => _react.default.createElement("label", {
+    htmlFor: id
+  }, label, _react.default.createElement("select", {
+    id: id,
+    value: state,
+    onChange: e => setState(e.target.value),
+    onBlur: e => setState(e.target.value),
+    disabled: options.legnth === 0
+  }, _react.default.createElement("option", null, "All"), options.map(item => _react.default.createElement("option", {
+    key: item,
+    value: item
+  }, item))));
 
   return [state, Dropdown, setState];
 };
@@ -32347,9 +32390,11 @@ exports.default = void 0;
 
 var _react = _interopRequireWildcard(require("react"));
 
-var _pet = require("@frontendmasters/pet");
+var _pet = _interopRequireWildcard(require("@frontendmasters/pet"));
 
-var _useDropdown = _interopRequireDefault(require("./useDropdown"));
+var _Results = _interopRequireDefault(require("./Results"));
+
+var _useDropdown5 = _interopRequireDefault(require("./useDropdown"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -32363,79 +32408,82 @@ function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = 
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-var SearchParams = function SearchParams() {
-  var _useState = (0, _react.useState)("Seattle, WA"),
-      _useState2 = _slicedToArray(_useState, 2),
-      location = _useState2[0],
-      setLocation = _useState2[1];
+const SearchParams = () => {
+  const _useState = (0, _react.useState)("Seattle, WA"),
+        _useState2 = _slicedToArray(_useState, 2),
+        location = _useState2[0],
+        setLocation = _useState2[1];
 
-  var _useState3 = (0, _react.useState)("dog"),
-      _useState4 = _slicedToArray(_useState3, 2),
-      animal = _useState4[0],
-      setAnimal = _useState4[1];
+  const _useState3 = (0, _react.useState)([]),
+        _useState4 = _slicedToArray(_useState3, 2),
+        breeds = _useState4[0],
+        setBreeds = _useState4[1];
 
-  var _useState5 = (0, _react.useState)(""),
-      _useState6 = _slicedToArray(_useState5, 2),
-      breed = _useState6[0],
-      setBreed = _useState6[1];
+  const _useDropdown = (0, _useDropdown5.default)("Animal", "dog", _pet.ANIMALS),
+        _useDropdown2 = _slicedToArray(_useDropdown, 2),
+        animal = _useDropdown2[0],
+        AnimalDropdown = _useDropdown2[1];
 
-  var _useState7 = (0, _react.useState)([]),
-      _useState8 = _slicedToArray(_useState7, 2),
-      breeds = _useState8[0],
-      setBreeds = _useState8[1];
+  const _useDropdown3 = (0, _useDropdown5.default)("Breed", "", breeds),
+        _useDropdown4 = _slicedToArray(_useDropdown3, 3),
+        breed = _useDropdown4[0],
+        BreedDropdown = _useDropdown4[1],
+        setBreed = _useDropdown4[2];
 
+  const _useState5 = (0, _react.useState)([]),
+        _useState6 = _slicedToArray(_useState5, 2),
+        pets = _useState6[0],
+        setPets = _useState6[1];
+
+  async function requestPets() {
+    const _ref = await _pet.default.animals({
+      location,
+      breed,
+      type: animal
+    }),
+          animals = _ref.animals;
+
+    setPets(animals || []);
+  }
+
+  ;
+  (0, _react.useEffect)(() => {
+    setBreeds([]);
+    setBreed("");
+
+    _pet.default.breeds(animal).then((_ref2) => {
+      let apiBreeds = _ref2.breeds;
+      const breedString = breeds.map((_ref3) => {
+        let name = _ref3.name;
+        return name;
+      });
+      setBreeds(breedString);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
   return _react.default.createElement("div", {
     className: "search-params"
-  }, _react.default.createElement("h1", null, location), _react.default.createElement("form", null, _react.default.createElement("label", {
+  }, _react.default.createElement("h1", null, location), _react.default.createElement("form", {
+    onSubmit: e => {
+      e.preventDefault();
+      requestPets();
+    }
+  }, _react.default.createElement("label", {
     htmlFor: "location"
   }, "Location", _react.default.createElement("input", {
     id: "location",
     value: location,
     placeholder: "location",
-    onChange: function onChange(e) {
-      return setLocation(e.target.value);
-    }
-  }), _react.default.createElement("button", {
+    onChange: e => setLocation(e.target.value)
+  })), _react.default.createElement(AnimalDropdown, null), _react.default.createElement(BreedDropdown, null), _react.default.createElement("button", {
     type: "submit"
-  }, "Submit")), _react.default.createElement("label", {
-    htmlFor: "animal"
-  }, "Animal", _react.default.createElement("select", {
-    id: "animal",
-    value: animal,
-    onChange: function onChange(e) {
-      return setAnimal(e.target.value);
-    },
-    onBlur: function onBlur(e) {
-      return setAnimal(e.target.value);
-    }
-  }, _react.default.createElement("option", null, "All"), _pet.ANIMALS.map(function (animal) {
-    return _react.default.createElement("option", {
-      key: animal,
-      value: animal
-    }, animal);
-  }))), _react.default.createElement("label", {
-    htmlFor: "breed"
-  }, "Breed", _react.default.createElement("select", {
-    id: "breed",
-    value: breed,
-    onChange: function onChange(e) {
-      return setBreed(e.target.value);
-    },
-    onBlur: function onBlur(e) {
-      return setBreed(e.target.value);
-    },
-    disabled: !breeds.length
-  }, _react.default.createElement("option", null, "All"), breeds.map(function (breedString) {
-    return _react.default.createElement("option", {
-      key: breedString,
-      value: breedString
-    }, breedString);
-  })))));
+  }, "Submit")), _react.default.createElement(_Results.default, {
+    pets: pets
+  }));
 };
 
 var _default = SearchParams;
 exports.default = _default;
-},{"react":"../node_modules/react/index.js","@frontendmasters/pet":"../node_modules/@frontendmasters/pet/index.js","./useDropdown":"useDropdown.js"}],"App.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js","@frontendmasters/pet":"../node_modules/@frontendmasters/pet/index.js","./Results":"Results.js","./useDropdown":"useDropdown.js"}],"App.js":[function(require,module,exports) {
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
@@ -32446,10 +32494,10 @@ var _SearchParams = _interopRequireDefault(require("./SearchParams"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var App = function App() {
-  return _react.default.createElement("div", null, _react.default.createElement("h1", {
+const App = () => {
+  return _react.default.createElement(_react.default.StrictMode, null, _react.default.createElement("div", null, _react.default.createElement("h1", {
     id: "something-important"
-  }, "Adopt Me!"), _react.default.createElement(_SearchParams.default, null));
+  }, "Adopt Me!"), _react.default.createElement(_SearchParams.default, null)));
 };
 
 (0, _reactDom.render)(_react.default.createElement(App, null), document.getElementById("root"));
@@ -32481,7 +32529,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50638" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49526" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
-import useDropdown from './useDropdown'
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
+import useDropdown from './useDropdown';
+
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState("dog");
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    })
+
+    setPets(animals || []);
+  };
+
+useEffect(() => {
+  setBreeds([]);
+  setBreed("");
+
+  pet.breeds(animal).then(({breeds: apiBreeds}) => {
+    const breedString = breeds.map(({name}) => name);
+    setBreeds(breedString)
+  },console.error)
+}, [animal, setBreed, setBreeds] );
 
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input
@@ -20,32 +46,12 @@ const SearchParams = () => {
             placeholder="location"
             onChange={e => setLocation(e.target.value)}
           />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
           <button type="submit">Submit</button>
-        </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={e => setAnimal(e.target.value)}
-            onBlur={e => setAnimal(e.target.value)}>
-              <option>All</option>
-              {ANIMALS.map(animal => <option key={animal} value={animal}>{animal}</option>)}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={e => setBreed(e.target.value)}
-            onBlur={e => setBreed(e.target.value)}
-            disabled={!breeds.length}>
-              <option>All</option>
-              {breeds.map(breedString => <option key={breedString} value={breedString}>{breedString}</option>)}
-          </select>
-        </label>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
