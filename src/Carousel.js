670c710f4588996fea8b6b10943f793693d5afec
hraya <hraya@lssu.edu>
2019-09-07 22:24:42 -0500

    Beginning the Carousel.js

---
+++ src/Carousel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Carousel extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default Carousel;
